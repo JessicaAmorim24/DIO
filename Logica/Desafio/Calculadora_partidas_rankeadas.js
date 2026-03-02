1ead1ce85b8691919ff639423402950263be9eed
JessicaAmorim24 <jessica-amorim24@hotmail.com>
2026-03-02 15:43:32 -0300

    Atualização da pasta Funcoes e Desafio

---
+++ Logica/Desafio/Calculadora_partidas_rankeadas.js
@@ -0,0 +1,44 @@
+// # 2️⃣ Calculadora de partidas Rankeadas
+// **O Que deve ser utilizado**
+
+// - Variáveis
+// - Operadores
+// - Laços de repetição
+// - Estruturas de decisões
+// - Funções
+
+// ## Objetivo:
+
+// Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
+// depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+
+// Se vitórias for menor do que 10 = Ferro
+// Se vitórias for entre 11 e 20 = Bronze
+// Se vitórias for entre 21 e 50 = Prata
+// Se vitórias for entre 51 e 80 = Ouro
+// Se vitórias for entre 81 e 90 = Diamante
+// Se vitórias for entre 91 e 100= Lendário
+// Se vitórias for maior ou igual a 101 = Imortal
+
+// ## Saída
+
+// Ao final deve se exibir uma mensagem:
+// "O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
+
+let vitorias = 215
+let derrotas = 35
+saldoVitorias = vitorias - derrotas
+
+function quantidadeVitorias(vitorias) {
+    if (vitorias <= 10) return "Ferro"
+    if (vitorias <= 20) return "Bronze"
+    if (vitorias <= 50) return "Prata"
+    if (vitorias <= 80) return "Ouro"
+    if (vitorias <= 90) return "Diamante"
+    if (vitorias <= 100) return "Lendário"
+    return "Imortal"
+}
+
+let nivel = quantidadeVitorias(saldoVitorias)
+
+console.log(`A heroína tem de saldo ${saldoVitorias} vitórias e está no nível ${nivel}`)
--- Logica/Funcoes/funcao_com_retorno.js
+++ Logica/Funcoes/funcao_com_retorno.js
@@ -1,6 +1,6 @@
 let resultado = soma (5, 5)
 
-console.log(" O resultado dessa funçõa é " + resultado)
+console.log("O resultado dessa função é " + resultado)
 
 function soma (numA, numB){
     let somatorio = numA + numB
@@ -11,7 +11,7 @@ function soma (numA, numB){
 
 resultado = soma (5, 5)
 
-console.log(" O resultado dessa funçõa é " + resultado)
+console.log("O resultado dessa função é " + resultado)
 
 function soma (numA, numB){
     return numA + numB
@@ -19,11 +19,20 @@ function soma (numA, numB){
 
 // Exemplo 2
 
-let userName = getFirstName("Carlos Almeida Juanito Gargalo")
+let userName = getFirstName("Carlos Almeida Juanito Gargalo", " ")
+    console.log("Seja bem vindo " + userName)
 
-console.log("Seja bem vindo " + userName)
+//function getFirstName(name){
+//    let firstName = name.split(" ")[0]
+//        return firstName
+//}    
 
-function getFirst (mae){
-    let firstName = namesplit (" ")[0]
-        return firstNAME
+// Exemplo 3
+
+userName = getFirstName("Felipe-Silva-Han-Solo", "-") // no lugar de deixar vazio tenho que colocar o traço, que é o separador do nome e sobrenome
+    console.log("Seja bem vindo " + userName)
+
+function getFirstName(name, splitChar = " "){ // splitChair é o separador em traço
+    let firstName = name.split(splitChar)[0]
+        return firstName
 }
